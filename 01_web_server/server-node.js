f680862776d2b7ba200af4d3cf60745610a7371e
dCOMMICS <sanwurld@gmail.com>
2024-07-10 22:27:14 +0300

    initial commit

---
+++ 01_web_server/server-node.js
@@ -0,0 +1,16 @@
+const http = require('http')
+
+const hostname = '127.0.0.1';
+const port = 8087;
+
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+
+    res.end('status connected');
+});
+
+server.listen(port, hostname, () =>{
+    console.log(`server listening on http://${hostname}:${port}`);
+});
