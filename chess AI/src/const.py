bb3ab6b11ea28977ab5d76163cd211e874660195
dCOMMICS <sanwurld@gmail.com>
2024-07-31 18:41:25 +0300

    💪🏿💯

---
+++ chess AI/src/const.py
@@ -0,0 +1,11 @@
+# scree dimesion
+
+width = 800
+height = 800
+
+#  board dimensions
+
+rows = 8
+cols = 8
+
+sqsize = width // cols
---
+++ chess AI/src/main.py
@@ -0,0 +1,28 @@
+# class Main: 
+    
+#     def __init__(self):
+#         pass
+    
+#     def mainloop(self):
+#         print ("katelong")
+    
+#     main = Main()
+    
+#     main.mainloop()
+import pygame
+import sys
+
+from const import *
+
+class Main:
+    def __init__(self):
+        pygame.init()
+    
+    def mainloop(self):
+        print("katelong")
+
+# Instantiate the Main class
+main = Main()
+
+# Call the mainloop method
+main.mainloop()
